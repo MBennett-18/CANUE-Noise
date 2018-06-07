2139cd448ff363f6906834ec8ade223b23cb9fca
MBennett-18 <39622417+MBennett-18@users.noreply.github.com>
2018-06-07 10:10:54 -0300

    Polygon_to csv changes and file rename

--- db_manip/polygon_to_csv.py
+++ db_manip/polygon_to_csv.py
@@ -5,6 +5,7 @@ import os
 # Changing Polygon predictors into a csv
 # NOTE that polygon predictors have TWO components, the shapefile and a Table
 # The shapefile gives the amount of area overlapped (i.e 50 km^2 is within the 100m buffer) and the table provides percentages per category (i.e of the 50 km^2 -> 20% is residential, 50% is commercial, and 30% are parks)
+# Tables are in excel format, no need for conversion
 #############
 # Indust (irr)
 # Land_use (lur)
