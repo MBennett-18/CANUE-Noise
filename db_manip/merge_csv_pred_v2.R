57e1e126f9b8bf896aef90ee726e980db48508ea
MBennett-18 <39622417+MBennett-18@users.noreply.github.com>
2018-06-22 07:57:35 -0300

    adding vancouver gtfs

--- db_manip/merge_csv_pred_v2.R
+++ db_manip/merge_csv_pred_v2.R
@@ -337,8 +337,54 @@ lng <- read.csv("D:/Work/Noise/aaa_CSV_CONVERTED/Predictors_CSV/2017/LNG/Merged/
 
 
 
+#########################################
+# VANCOUVER 2003 GTFS ROUTES/STOPS
+#########################################
 
+inPath = "D:/Work/Noise/aaa_CSV_CONVERTED/Predictors_CSV/2003/VAN/GTFS_dat"
+outPath = "D:/Work/Noise/aaa_CSV_CONVERTED/Predictors_CSV/2003/VAN/Merged/"
+outName = "van2003_gtfs.csv"
 
+# Working Directory
+setwd(inPath)
+
+filenames <- list.files(pattern="*.csv", recursive = TRUE)
+
+all_files <- lapply(filenames, function(x) {
+  file <- read.csv(x)
+  # Get the start of filename prefix
+  prefix1 = sub("_.*", "", x)
+  prefix = sub("/.*", "", prefix1)
+  # Get the suffix number
+  suffix1 = sub(".*_", "", x)
+  suffix = str_extract(suffix1, '^[^.]+')
+  colnames(file) <- paste(colnames(file), prefix, suffix, sep='_')
+  colnames(file) <- sub("PCA_ID.*", "PCA_ID", colnames(file))
+  return(file)
+})
+
+# Function to join all data in list,
+# Agruments (list of data, join function type, ... = specify by = "colname")
+multi_join <- function(list.data, join_func, ...){
+    require("dplyr")
+    output <- Reduce(function(x, y) {join_func(x, y, ...)}, list.data)
+    return(output)
+}
+
+# Run the previuosly defined function to join all within list
+merged_data <- multi_join(all_files, full_join, by = "PCA_ID")
+# List of variables to keep, note distance is kept to ensure proper colname l;abel
+voi <- c("PCA_ID",
+          "POSTALCODE",
+          "distance",
+          "Point_Coun",
+          "SUM_count",
+          "SUM_Length",
+          "SUM_leng_1")
+# Only keep vars that are contained within the voi list
+merged_drop <- merged_data[, grep(paste(voi,collapse="|"),colnames(merged_data))]
+# Export full db to csv
+write.csv(merged_drop, file.path(outPath, outName))
 
 #########################################
 # HALIFAX 2010 GTFS ROUTES/STOPS
